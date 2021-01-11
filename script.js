a87f6f715a1c028d461633046c6480277dabab00
Harsh01123 <67332766+Harsh01123@users.noreply.github.com>
2021-01-11 08:42:31 +0530

    creating a copy of lets chat

---
+++ script.js
@@ -0,0 +1,416 @@
+
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyDxKFPOTR8_ACeN0-Yp1iEzzKZUEVHIx4k",
+    authDomain: "let-s-chat-ae4f8.firebaseapp.com",
+    databaseURL: "https://let-s-chat-ae4f8.firebaseio.com",
+    projectId: "let-s-chat-ae4f8",
+    storageBucket: "let-s-chat-ae4f8.appspot.com",
+    messagingSenderId: "446016550617",
+    appId: "1:446016550617:web:efbe17e9b64a62d16b70c8"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+// my app
+ 
+
+
+
+//form validation
+
+//first user name
+
+
+
+
+
+//another user name
+
+
+
+
+
+
+
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+var myName = prompt("type your username ");
+var emoji 
+var box = document.querySelector(".boxx");
+
+function aa() {
+  emoji = "&#128512;"
+  message.value += (" " + emoji);
+};
+function ab() {
+  emoji = "&#128513;"
+  message.value += (" " + emoji);
+};
+function ac() {
+  emoji = "&#128514;"
+  message.value += (" " + emoji);
+};
+function ad() {
+  emoji = "&#129315;"
+  message.value += (" " + emoji);
+};
+function ae() {
+  emoji = "&#128515;"
+  message.value += (" " + emoji);
+};
+function af() {
+  emoji = "&#128516;"
+  message.value += (" " + emoji);
+};
+function ag() {
+  emoji = "&#128518;"
+  message.value += (" " + emoji);
+};
+function ah() {
+  emoji = "&#128517"
+  message.value += (" " + emoji);
+};
+function ai() {
+  emoji = "&#128521;"
+  message.value += (" " + emoji);
+};
+function aj() {
+  emoji = "&#128522;"
+  message.value += (" " + emoji);
+};
+function ak() {
+  emoji = "&#128523;"
+  message.value += (" " + emoji);
+};
+function al() {
+  emoji = "&#128526;"
+  message.value += (" " + emoji);
+};
+function am() {
+  emoji = "&#128525;"
+  message.value += (" " + emoji);
+};
+function an() {
+  emoji = "&#128536;"
+  message.value += (" " + emoji);
+};
+function ao() {
+  emoji = "&#129300;"
+  message.value += (" " + emoji);
+};
+function ap() {
+  emoji = "&#128578;"
+  message.value += (" " + emoji);
+};
+function ar() {
+  emoji = "&#129303;"
+  message.value += (" " + emoji);
+};
+function as() {
+  emoji = "&#128528;"
+  message.value += (" " + emoji);
+};
+function at() {
+  emoji = "&#128529;"
+  message.value += (" " + emoji);
+};
+function au() {
+emoji = "&#128566;"
+message.value += (" " + emoji);
+};
+function av() {
+  emoji = "&#128580;"
+  message.value += (" " + emoji);
+};
+function aw() {
+  emoji = "&#128527;"
+  message.value += (" " + emoji);
+};
+function ax() {
+  emoji = "&#128547;"
+  message.value += (" " + emoji);
+};
+function ay() {
+  emoji = "&#128549;"
+  message.value += (" " + emoji);
+};
+function az() {
+  emoji = "&#128558;"
+  message.value += (" " + emoji);
+};
+function aaa(){
+  emoji = "&#129296;"
+  message.value += (" " + emoji);
+};
+function aab(){
+  emoji = "&#128559;"
+  message.value += (" " + emoji);
+};
+function aac(){
+  emoji = ""
+  message.value += (" " + emoji);
+};
+function aad(){
+  emoji = ""
+  message.value += (" " + emoji);
+};
+function aae(){
+  emoji = ""
+  message.value += (" " + emoji);
+};
+function aaf(){
+  emoji = ""
+  message.value += (" " + emoji);
+};
+function aag(){
+  emoji = ""
+  message.value += (" " + emoji);
+};
+function aah(){
+  emoji = ""
+  message.value += (" " + emoji);
+};
+function aai(){
+  emoji = ""
+  message.value += (" " + emoji);
+};
+function aaj(){
+  emoji = ""
+  message.value += (" " + emoji);
+};
+function aak(){
+  emoji = ""
+  message.value += (" " + emoji);
+};
+function aal(){
+  emoji = ""
+  message.value += (" " + emoji);
+};
+function aam(){
+  emoji = ""
+  message.value += (" " + emoji);
+};
+function aan(){
+  emoji = ""
+  message.value += (" " + emoji);
+};
+function aao(){
+  emoji = ""
+  message.value += (" " + emoji);
+};
+function aap(){
+  emoji = ""
+  message.value += (" " + emoji);
+};
+function aaq(){
+  emoji = ""
+  message.value += (" " + emoji);
+};
+function aar(){
+  emoji = ""
+  message.value += (" " + emoji);
+};
+function aas(){
+  emoji = ""
+  message.value += (" " + emoji);
+};
+function aat(){
+  emoji = ""
+  message.value += (" " + emoji);
+};
+function aau(){
+  emoji = ""
+  message.value += (" " + emoji);
+};
+function aax(){
+  emoji = ""
+  message.value += (" " + emoji);
+};
+function aay(){
+  emoji = ""
+  message.value += (" " + emoji);
+};
+function aaz(){
+  emoji = ""
+  message.value += (" " + emoji);
+};
+function aba(){
+  emoji = ""
+  message.value += (" " + emoji);
+};
+function abb(){
+  emoji = ""
+  message.value += (" " + emoji);
+};
+function abc(){
+ emoji = ""
+ message.value += (" " + emoji);
+};
+function abd(){
+  emoji = ""
+  message.value += (" " + emoji);
+};
+function abe(){
+  emoji = ""
+  message.value += (" " + emoji);
+};
+function abf(){
+  emoji = ""
+  message.value += (" " + emoji);
+};
+function abg(){
+  emoji = ""
+  message.value += (" " + emoji);
+};
+function abh(){
+  emoji = ""
+  message.value += (" " + emoji);
+};
+function abi(){
+  emoji = ""
+  message.value += (" " + emoji);
+};
+function abj(){
+  emoji = ""
+  message.value += (" " + emoji);
+};
+function abk(){
+  emoji = ""
+  message.value += (" " + emoji);
+};
+function abl(){
+  emoji = ""
+  message.value += (" " + emoji);
+};
+function abm(){
+  emoji = ""
+  message.value += (" " + emoji);
+};
+function abn(){
+  emoji = ""
+  message.value += (" " + emoji);
+};
+function abo(){
+  emoji = ""
+  message.value += (" " + emoji);
+};
+function abp(){
+  emoji = ""
+  message.value += (" " + emoji);
+};
+function abq(){
+  emoji = ""
+  message.value += (" " + emoji);
+};
+function abr(){
+  emoji = ""
+  message.value += (" " + emoji);
+};
+function abs(){
+  emoji = ""
+  message.value += (" " + emoji);
+};
+function abt(){
+  emoji = ""
+  message.value += (" " + emoji);
+};
+function abu(){
+  emoji = ""
+  message.value += (" " + emoji);
+};
+function abv(){
+  emoji = ""
+  message.value += (" " + emoji);
+};
+function abw(){
+  emoji = ""
+  message.value += (" " + emoji);
+};
+function abx(){
+  emoji = ""
+  message.value += (" " + emoji);
+};
+function aby(){
+  emoji = ""
+  message.value += (" " + emoji);
+};
+function abz(){
+  emoji = ""
+  message.value += (" " + emoji);
+};
+function aca(){
+  emoji = ""
+  message.value += (" " + emoji);
+};
+
+
+var date = new Date();
+var mins = date.getMinutes();
+var hours = date.getHours() % 12 || 12;
+var time = hours + ":" + mins;
+
+
+function sendmessage(){
+  //message
+  var message = document.getElementById("message").value;
+  //save to data base
+  firebase.database().ref("messages").push().set({
+    "sender": myName,
+    "message": message,
+    "time": time
+  });
+document.getElementById("message").value = "";
+  //false retrun
+  return false;
+}
+firebase.database().ref("messages").on("child_added",function(snapshot){
+ var html="";
+
+ html += "<div>";
+  html += "<section>";
+    html += "<small><h1>" + snapshot.val().sender + "</h1></small><big><h2 id='message-"+ snapshot.key +"'><br>" + snapshot.val().message + "</h2></big>";
+    html += "<h3><br>" + snapshot.val().time + "</h3>";
+      html += "<button data-id='" + snapshot.key +"' onclick='deleteMessage(this);'>";
+      html += "<i class='new fa fa-trash' aria-hidden='true'></i>"
+      html += "</button>";
+  html += "</section>";
+ html += "</div>";
+
+ document.getElementById("messages").innerHTML += html; 
+
+ return true;
+});
+function deleteMessage(self) {
+  var messageId = self.getAttribute("data-id");
+  firebase.database().ref("messages").child(messageId).remove();
+  firebase.database().ref("messages").on("child_removed", function (snapshot) {
+    document.getElementById("message-" + snapshot.key).innerHTML = "This Message Is Deleted";
+  });
+}
+
